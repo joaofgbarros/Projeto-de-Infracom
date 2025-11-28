1c9e281304c33034019a5fe271724c4b4af846a7
Kilezico <jgrangeirocosta@gmail.com>
2025-11-27 23:53:37 -0300

    Cria enviador RDT

---
+++ Entrega2/rdt.py
@@ -0,0 +1,38 @@
+from socket import *
+
+class rdt_sender:
+    acks = [b'ACK0', b'ACK1']
+    pkts = [b'PKT0', b'PKT1']
+    buffer_size = 1024
+
+    def __init__(self, socket: socket):
+        self.socket = socket
+        self.seqnum = 0
+    
+    def send_bytes(self, data, dest):
+        # Envia os chunks
+        self.socket.settimeout(0.5)
+        for i in range(0, len(data), type(self).buffer_size-4):
+            # Chunk dos dados + número de sequência
+            msg = type(self).pkts[self.seqnum] + data[i:i+type(self).buffer_size-4]
+            self.send(msg, dest)
+            print("Recebi o ACK bom >:D")
+            self.seqnum = 1 - self.seqnum
+        self.send(type(self).pkts[self.seqnum], dest)
+
+    def send(self, msg, dest):
+        self.udt_send(msg, dest)
+        recvd = False
+        while not recvd:
+            # Assume que enviou. Agora, quero o meu ACK.
+            try:
+                ack = self.socket.recv(type(self).buffer_size)
+                recvd = (ack == type(self).acks[self.seqnum])
+            except:
+                print("Timeout! Retransmitindo...")
+                self.udt_send(msg, dest)
+
+
+    def udt_send(self, data, dest):
+        # falta adicionar o random :-)
+        self.socket.sendto(data, dest)
